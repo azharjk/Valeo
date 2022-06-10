dc6c48b0aa6b07f4ae213b819cf245fb27cb2a97
azharjk <azhr.project@gmail.com>
2022-06-10 09:12:48 +0700

    Add basic expression tests

--- tests/app_test.py
+++ tests/app_test.py
@@ -7,6 +7,31 @@ class AppTest(unittest.TestCase):
         result = valeo_debug_string('')
         self.assertEqual(result, None)
 
+    def test_single_number_in_paren(self):
+        result = valeo_eval('(10)')
+        self.assertEqual(result, 10)
+
+    def test_only_paren(self):
+        result = valeo_eval('()')
+        self.assertEqual(result, None)
+
+    def test_paren_follow_by_expr(self):
+        result = valeo_eval('10+(1)')
+        self.assertEqual(result, 11)
+
+    def test_basic_expression(self):
+        r1 = valeo_eval('100+50')
+        self.assertEqual(r1, 150)
+
+        r2 = valeo_eval('65+89*2')
+        self.assertEqual(r2, 243)
+
+        r3 = valeo_eval('800/400*72-2')
+        self.assertEqual(r3, 142)
+
+        r4 = valeo_eval('(8+8)*25')
+        self.assertEqual(r4, 400)
+
     @unittest.skip('Not implemented yet')
     def test_whitespaces_input(self):
         result1 = valeo_eval('1 + 1')
